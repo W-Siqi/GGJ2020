5f41ce5da8dd58e6daf128af10c3142929270acd
SiqiWang <wangsiqi_mailbox@163.com>
2020-01-31 22:49:14 -0500

    moving platform

---
+++ goast-become-human/Assets/Scripts/Siqi/MovingPlatform.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    public float speed =1f;
+    public float moveRange = 1f;
+    public bool isMoving = true;
+
+    private float left;
+    private float right;
+    private bool towardsLeft;
+
+    private void Start()
+    {
+        towardsLeft = true;
+        left = transform.position.x - moveRange;
+        right = transform.position.x + moveRange;
+    }
+
+    private void Update()
+    {
+        if (isMoving) {
+            if (towardsLeft)
+            {
+                transform.Translate(Vector3.left * Time.deltaTime * speed);
+                if (transform.position.x < left)
+                    towardsLeft = false;
+            }
+            else {
+                transform.Translate(Vector3.right * Time.deltaTime * speed);
+                if (transform.position.x > right)
+                    towardsLeft = true;
+            }
+        }
+    }
+}
--- goast-become-human/Assets/Scripts/Siqi/SpawnManager.cs
+++ goast-become-human/Assets/Scripts/Siqi/SpawnManager.cs
@@ -4,15 +4,7 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public class Config {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
