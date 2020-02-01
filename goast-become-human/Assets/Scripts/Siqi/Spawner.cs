be53d8ed036a7c063a691500a3dd374807c95139
SiqiWang <wangsiqi_mailbox@163.com>
2020-01-31 21:54:44 -0500

    Update Spawner.cs

--- goast-become-human/Assets/Scripts/Siqi/Spawner.cs
+++ goast-become-human/Assets/Scripts/Siqi/Spawner.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public float spwanStartTime = 0f;
+    public int maxCount = 3;
+    public Vector2 spawnInterval = new Vector2(2f, 3f);
+    public Vector2 spawnRange = new Vector2(1f, 1f);
+    public GameObject spawnPrefab = null;
+
+    IEnumerator SpawnLoop() {
+        yield return new WaitForSeconds(spwanStartTime);
+
+        while (true) {
+            if (isUnderCount) {
+                var spawnPos = GetSpawnPos();
+                GameObject.Instantiate(spawnPrefab, spawnPos, Quaternion.identity);
+            }
+            var interval = Random.Range(spawnInterval.x, spawnInterval.y);
+            yield return new WaitForSeconds(interval);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private Vector3 GetSpawnPos() {
+        var randomBias = new Vector3(Random.Range(-spawnRange.x, spawnRange.x),
+            Random.Range(-spawnRange.y, spawnRange.y),0);
+        return transform.position + randomBias;
+    }
+
+    private bool isUnderCount { get { return true; } }
+
+    private void OnDrawGizmos()
     {
-        
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(transform.position, 0.3f);
     }
 }
