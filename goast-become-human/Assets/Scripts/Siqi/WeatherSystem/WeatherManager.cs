25fe0256e5b57af737eecacf25ec8bc15475afb6
SiqiWang <wangsiqi_mailbox@163.com>
2020-02-02 10:05:08 -0500

    weather

--- goast-become-human/Assets/Scripts/Siqi/WeatherSystem/WeatherManager.cs
+++ goast-become-human/Assets/Scripts/Siqi/WeatherSystem/WeatherManager.cs
@@ -6,7 +6,12 @@ using UnityEngine.Networking;
 [RequireComponent(typeof(LocationGetter))]
 public class WeatherManager : MonoBehaviour
 {
+    const float SNOW_TEMP = 280;
     const string API_KEY = "9951994425c9141f94b240d61ba45152";
+
+    public string initCityName = "Pittsburgh";
+    public GameObject snowEffect = null;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,7 +19,7 @@ public class WeatherManager : MonoBehaviour
     }
 
     IEnumerator InitWeather() {
-        var wr = RequestWeather();
+        var wr = RequestWeather(initCityName);
         wr.SendWebRequest();
         while (true) {
             yield return null;
@@ -26,15 +31,23 @@ public class WeatherManager : MonoBehaviour
         }
     }
 
-    UnityWebRequest RequestWeather() {
+    UnityWebRequest RequestWeather(string cityName) {
         WWW myWww = new WWW("https://samples.openweathermap.org/data/2.5/weather?id=2177535&appid=9951994425c9141f94b240d61ba45152");
         // ... is analogous to ...
-        UnityWebRequest myWr = UnityWebRequest.Get("https://samples.openweathermap.org/data/2.5/weather?id=2177535&appid=9951994425c9141f94b240d61ba45152");
+        var url = string.Format("https://api.openweathermap.org/data/2.5/weather?q={0}&appid={1}",cityName,API_KEY);
+        UnityWebRequest myWr = UnityWebRequest.Get(url);
         return myWr;
     }
 
     void ChangeSceneWeather(WeatherInfo weatherInfo) {
         Debug.Log("weather: " + weatherInfo.weatherType);
         Debug.Log("temp: " + weatherInfo.temperature);
+        if (weatherInfo.temperature < SNOW_TEMP)
+        {
+            snowEffect.SetActive(true);
+        }
+        else {
+            snowEffect.SetActive(false);
+        }
     }
 }
