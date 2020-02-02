22a841948758a147e13ed2efdc53ec5e5af3efed
SiqiWang <wangsiqi_mailbox@163.com>
2020-02-02 00:04:53 -0500

    weason and tutorial

---
+++ goast-become-human/Assets/Scripts/Siqi/Tutorial/TutorialFinishTrriger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialFinishTrriger : MonoBehaviour
+{
+    public bool isReady = false;
+	public string triggerTag = "player";
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == triggerTag) {
+            isReady = true;
+        }
+    }
+}
---
+++ goast-become-human/Assets/Scripts/Siqi/Tutorial/TutorialManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialManager : MonoBehaviour
+{
+    public string sceneName = "FinalScene";
+    public TutorialFinishTrriger[] finishTrrigers;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        finishTrrigers = FindObjectsOfType<TutorialFinishTrriger>();
+        StartCoroutine(TutorialEndCheck());
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
+        }
+    }
+
+    IEnumerator TutorialEndCheck() {
+        yield return null;
+        while (true) {
+            var finished = true;
+            foreach (var tri in finishTrrigers) {
+                if (!tri.isReady)
+                    finished = false;
+            }
+
+            if (finished)
+                UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
+        }
+    }
+}
