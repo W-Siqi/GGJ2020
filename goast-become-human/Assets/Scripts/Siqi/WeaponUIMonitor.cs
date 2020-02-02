7a3bf0c454221c6d6474f64cf6fb04a5984a6fdd
SiqiWang <wangsiqi_mailbox@163.com>
2020-02-02 11:49:18 -0500

    weapon UI

---
+++ goast-become-human/Assets/Scripts/Siqi/WeaponUIMonitor.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponUIMonitor : MonoBehaviour
+{
+    public CharacterInfo characterInfo;
+    public GameObject UI;
+
+    private void Update()
+    {
+        if (characterInfo.equippedItem != ItemInfo.ItemType.None)
+            UI.SetActive(true);
+        else
+            UI.SetActive(false);
+    }
+}
