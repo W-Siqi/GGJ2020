e75652ac6db08127a9be195216beb8089af2a1d2
SiqiWang <wangsiqi_mailbox@163.com>
2020-02-02 12:44:28 -0500

    win scene

--- goast-become-human/Assets/Scripts/Siqi/WinSceneManager.cs
+++ goast-become-human/Assets/Scripts/Siqi/WinSceneManager.cs
@@ -5,6 +5,25 @@ using UnityEngine;
 public class WinSceneManager : MonoBehaviour
 {
     public string startSceneName = "StartScene";
+
+    public GameObject player1Win;
+    public GameObject player2Win;
+
+    private IEnumerator Start()
+    {
+        yield return new WaitForSeconds(1f);
+        var winplayer = WinSceneConditionHandle.ReadHandleAndDestroy();
+        if (winplayer == WinSceneConditionHandle.WinPlayer.player1)
+        {
+            player1Win.SetActive(true);
+            player2Win.SetActive(false);
+        }
+        else {
+            player1Win.SetActive(false);
+            player2Win.SetActive(true);
+        }
+    }
+
     // Update is called once per frame
     void Update()
     {
